2e98c716b375cbbf70895d1903915f0ebeade3c5
raghavchandak <raghavchandak25@gmail.com>
2022-09-12 19:06:20 -0600

    Updated scripts

--- req.py
+++ req.py
@@ -2,4 +2,4 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get("https://www.google.com/"))
+print(requests.get("https://raw.githubusercontent.com/raghavchandak/Cmput-404/main/req.py").text)
